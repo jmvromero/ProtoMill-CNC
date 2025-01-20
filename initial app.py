5b5e182bae069a2646fc7a2bc8d843424ab6ee95
jmvromero <johnmichaelvincentpromero@iskolarngbayan.pup.edu.ph>
2025-01-20 22:56:57 +0800

    With LCD function

--- initial app.py
+++ initial app.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 import subprocess  # Import subprocess to run the Streamlit app
+from RPLCD.i2c import CharLCD
 
 
 # Appearance mode (It could be Dark or Light based on the system)
@@ -15,6 +16,14 @@ app.iconbitmap("cnc-machine.ico")
 app.title("ProtoMill CNC")
 app.geometry("480x320")  # Start with a smaller window size (480x320)
 app.resizable(False, False)  # Allow window resizing
+
+# Initialize the LCD and display a welcome message
+lcd = CharLCD('PCF8574', 0x27)
+lcd.write_string("Welcome to")
+lcd.crlf()
+lcd.write_string("ProtoMill CNC")
+lcd.crlf()
+lcd.backlight_enabled = True
 
 # Load the background image (1920x1080)
 bg_image_path = "background.png"  # Replace with your actual image path
@@ -47,6 +56,11 @@ def launch_bcnc():
 def launch_defect_detection():
     subprocess.Popen(["streamlit", "run", "app.py"])
 
+# Function to launch FlatCAM
+
+
+
+
 
 # Define bCNCButton with command tied to launch_bCNC
 bCNCButton = ctk.CTkButton(content_frame, text="bCNC", font=("Montserrat", 15), text_color="black",
@@ -62,8 +76,12 @@ inspectMillButton = ctk.CTkButton(content_frame, text="Defect Detection", font=(
                                   border_color="#060270", hover_color="#CECECE",
                                   border_width=2, image=ctk.CTkImage(dark_image=ddImg, light_image=ddImg),
                                   command=launch_defect_detection)  # Command to launch the defect detection app
+
+
 inspectMillButton.place(relx=0.5, rely=0.7, anchor="center")
 
 
+launch_bcnc()  # Automatically start bCNC
+launch_defect_detection()  # Automatically start defect detection
 # Run the application
 app.mainloop()
